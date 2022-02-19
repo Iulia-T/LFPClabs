d3258e86f35c5ea0fb6c1776af648a3be32d48c6
Iulia-T <72933695+Iulia-T@users.noreply.github.com>
2022-02-19 08:46:53 +0200

    Update lab2.java

--- Laboratory2/lab2.java
+++ Laboratory2/lab2.java
@@ -1,18 +1,8 @@
-package Laboratory2;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Scanner;
-
-public class lab2 {
-
-    static String rule;
-    static int nonterminals, terminals;
-    static char[] listOfTerminals = new char[20];
-    static int currentState;
-    static String str;
+    static boolean mapContainsKey;
+    static String[][] table1;
 
     static HashMap<String, String[]> nfaTable = new HashMap<String, String[]>();
+    static HashMap<String, String[]> dfaTable = new HashMap<String, String[]>();
 
 
 ////////////////////////////////////////////////main///////////////////////////////////////////////////////
@@ -28,7 +18,6 @@ public class lab2 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter terminal points: ");
         String t = sc.nextLine();
-        //System.out.println(t);
 
         terminals = 0;
         for (int i = 0; i < t.length(); i++)
@@ -36,8 +25,6 @@ public class lab2 {
                 listOfTerminals[terminals] = t.charAt(i);
                 terminals++;
             }
-        //for (int i = 0; i < terminals; i++)
-        //System.out.print(listOfTerminals[i] + " ");
 
         System.out.println("Introduce rules line by line (ex: q0,a = q1). Finish by entering STOP.");
 
@@ -46,18 +33,20 @@ public class lab2 {
             rule = in.nextLine();
             if (rule.equals("STOP")) break;
             else tableNFA(rule);
-
-
-
-
         }
+
+        drawNFAtable();
+
+        conversion(table1);
+
+
 
     }
 
 
 /////////////////////////////////////////Building NFA table//////////////////////////////////////////////
     static void tableNFA (String rule){
-        String key = rule.substring(0,2);
+        key = rule.substring(0,2);
         String[] value = new String[terminals];
         String assignedNonterminal = null;
 
@@ -91,35 +80,85 @@ public class lab2 {
                                 break;
                             }
                         if (b) currentValue += assignedNonterminal;
-                        //value[currentState] = sortingString(value[currentState]);
                     }
                     value[currentState] = currentValue;
                     nfaTable.put(key, value);
 
                  }
-                String x = Arrays.toString(value);
-                System.out.println(key + " "+ x);
+                //System.out.println(key + " "+ Arrays.toString(value));
             }
         }
 
     }
 
 
-        static String sortingString(String str){
-            String str1 = null;
-            String str2 = null;
-            String str3 = null;
-            String str4 = null;
+//////////////////////////////////Draw NFA Table//////////////////////////////////////////////////
+    static void drawNFAtable(){
+        //create NFA Table
+        table1 = new String[nonterminals+1][terminals+1];
+        table1[0][0] = " ";
+        for (int i = 1; i < terminals+1; i++)
+            table1[0][i] = String.valueOf(listOfTerminals[i-1]);
+        for (int i = 1; i < nonterminals+1; i++) {
+            table1[i][0] = "q".concat(String.valueOf((char)((i - 1) + 48)));
+            for (int j = 1; j < terminals+1; j++)
+                table1[i][j] = nfaTable.get(table1[i][0])[j-1];
 
-            for (int j = 3; j < str.length(); j += 2)
-                for (int k = 3; k < str.length(); k += 2)
-                    if (str.charAt(k) < str.charAt(k-2)) {
-                        if (k > 3) str1 = str.substring(0, k-3);
-                        str2 = str.substring (k-1, k+1);
-                        str3 = str.substring (k-3, k-1);
-                        if (str.length() > k+1) str4 = str.substring (k+1);
-                    }
-            return str1+str2+str3+str4;
+        }
+    }
+
+
+//////////////////////////////////Convert from NFA to DFA/////////////////////////////////////////////
+    static void conversion(String[][] table1) {
+
+        String[] valueOfDFA = new String[terminals];
+        for (int i = 0; i < terminals; i++)
+            valueOfDFA[i] = String.valueOf(listOfTerminals[i]);
+        dfaTable.put(" ", valueOfDFA); //the first line represents the terminal points
+
+        for (int i = 0; i < terminals; i++) {
+            valueOfDFA[i] = table1[1][i+1];
+            dfaTable.put("q0", valueOfDFA); //this lined is just copied from NFA
         }
 
-}
+        int currentRow = 1;
+
+        mapContainsKey = false;
+        while (!mapContainsKey && valueOfDFA[0]!=null){
+            constructorDFA(valueOfDFA);
+            currentRow++;
+        }
+
+    }
+
+    static void constructorDFA(String[] valueOfDFA){
+
+        String[] auxiliarValue = new String[terminals];
+        String str;
+        String key = null;
+
+        for (int i = 0; i < terminals; i++){
+            mapContainsKey = false;
+            if (!dfaTable.containsKey(valueOfDFA[i])){
+                key = valueOfDFA[i];
+                for (int j = 0; j < key.length()/2; j++){
+                    str = key.substring(j, j + 2);
+                    for (int path = 0; path < terminals; path++){
+
+                        //see if elements don't repeat in a string
+                        boolean b = true;
+                        for (int k = 1; k < nfaTable.get(str)[path].length(); j += 2)
+                            if (auxiliarValue[path].charAt(k) == str.charAt(1)) {
+                                b = false;
+                                break;
+                            }
+                        if (b) auxiliarValue[path] += nfaTable.get(str)[path];
+                    }
+                }
+                mapContainsKey = true;
+            }
+            else mapContainsKey = true;
+        }
+        dfaTable.put(key,auxiliarValue);
+
+    }
